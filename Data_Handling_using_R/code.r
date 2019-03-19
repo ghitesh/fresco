6d403d3087617ee1dd76052a26467c30a670137a
ghitesh <ghitesh@gmail.com>
2019-03-19 14:36:01 +0530

    Update code.r

--- Data_Handling_using_R/code.r
+++ Data_Handling_using_R/code.r
@@ -1,17 +1,18 @@
+#Data Handling Using R -1- Data Frames in R 
+
 name <- c("Ankur","Bharti","Chintan","Devansh","Elvis")
 age <- c(28,30,32,34,18)
 department <- c ("IT","Advisory","IT",NA,"Music")
 empdetails <- data.frame(name,age,department)
-
 empdetails
 str(empdetails)
 
-
+#Sets
 
 library(help = datasets)
 ?mtcars
 str(mtcars)
 summary(mtcars)
 
-
+#Practice Exercise on Data Frame Operations
 mtcars [,c(1,2,10)]
